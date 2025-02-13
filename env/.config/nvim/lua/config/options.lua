7d1a34fa4b9d47ee6a90cfe2ee8b9aab03891caf
dhinogz <davidhinojosagzz@gmail.com>
2025-02-13 16:30:22 -0600

    Add tab options and refactor victim files

--- env/.config/nvim/lua/config/options.lua
+++ env/.config/nvim/lua/config/options.lua
@@ -2,3 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.cursorline = false
+
+vim.opt.scrolloff = 10
