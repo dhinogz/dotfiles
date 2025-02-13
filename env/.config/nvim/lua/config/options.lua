736b8cab4394bdeb42376d32dbbd63f1c983dda2
dhinogz <davidhinojosagzz@gmail.com>
2025-02-13 17:56:31 -0600

    Remove swapfile support

--- env/.config/nvim/lua/config/options.lua
+++ env/.config/nvim/lua/config/options.lua
@@ -11,3 +11,8 @@ vim.opt.expandtab = true
 vim.opt.cursorline = false
 
 vim.opt.scrolloff = 10
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
