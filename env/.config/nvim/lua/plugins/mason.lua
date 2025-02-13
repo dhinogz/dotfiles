2ed9bef27497f00e51809d26f6221c2b119cb1b9
dhinogz <davidhinojosagzz@gmail.com>
2025-02-13 15:11:47 -0600

    Add templ LSP

---
+++ env/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    event = "LazyFile",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "templ",
+      },
+    },
+  },
+}
